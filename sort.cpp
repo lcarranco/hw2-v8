3c3e83209a7c2f58fd87a9bb0bf75b1382beaaa7
lcarranco <lcarranco@gmail.com>
2017-03-03 14:08:19 -0600

    Added merge function to Sort class
    Added more things to Sort class

--- sort.cpp
+++ sort.cpp
@@ -240,6 +240,10 @@ public:
         std::swap(isNegative, other.isNegative);
         data.swap(other.data);
     }
+     bool operator>= (BigNumber const & other) const
+    {
+        return !operator< (other);
+    }
 
 private:
     bool isNegative = false;
@@ -391,6 +395,72 @@ public:
       bigO<< "insert" <<"        "<< size-1 <<"               "<< countComparisons<< "            "<< countSwaps <<endl;
     }
 
+    
+void merge(BigNumber *a, int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    //Create temp arrays
+    BigNumber L[n1];
+    BigNumber R[n2];
+    //Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+    {
+        L[i]=(a[l + i]);
+    }
+    for (j = 0; j < n2; j++)
+    {
+        R[j]=(a[m + 1 + j]);
+    }
+    //Merge the temp arrays back into arr[l...r]
+    i = 0; //Initial index of first subarray
+    j = 0; //Initial index of second subarray
+    k = l; //Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (R[j] >= L[i])
+        {
+            a[k]=(L[i]);
+            i++;       
+        }
+        else
+        {
+            a[k]=(R[j]);
+            j++;
+        }
+        k++;
+    }
+    //Copy the remaining elements of L[], if there are any
+    while (i < n1)
+    {
+        a[k]=(L[i]);
+        i++;
+        k++;
+    }
+    //Copy the remaining elents of R[], if there are any
+    while (j < n2)
+    {
+        a[k]=(R[j]);
+        j++;
+        k++;
+    }
+}
+
+//l is for left index and r is right index of the sub-array of arr to be sorted
+void mergeSort(BigNumber *a, int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - 1) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+
+
+
     bool open_the_bigO()
       {
 
@@ -423,9 +493,17 @@ public:
     // }
 
     fstream bigO;
+    Sort & operator= (Sort & other)
+    {
+        // cout << "operator=" << endl;
+        a = other.a;
+        //data = other.data;
+        return *this;
+    }
 
   private:
       //fstream bigO;
+      BigNumber *a;
 
   };
 
@@ -510,7 +588,8 @@ int main(int argc, char* argv[])
     }
     if (algorithm == "merge")
     {
-
+        sort.mergeSort(a,0,size-1);
+        print(a,size);
     }
     if (algorithm == "heap")
     {
